b6829603248a17348143bae4cac833bb7110ed64
DjNoopNoop <rajeevan.anoop@gmail.com>
2026-02-24 14:10:31 -0500

    added tom-select for geographic location

--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -14,7 +14,8 @@ class PublicationsController < ApplicationController
   end
 
   def create
-    @publication = Publication.new(publication_params)
+    @publication = Publication.new(assignment_attrs)
+    sanitize_geographic_location(@publication)
     apply_tag_creatable_associations(@publication)
 
     if @publication.save
@@ -27,10 +28,8 @@ class PublicationsController < ApplicationController
   def edit; end
 
   def update
-    puts "*"*100
-    puts publication_params
-    puts "-"*100
-    @publication.assign_attributes(publication_params)
+    @publication.assign_attributes(assignment_attrs)
+    sanitize_geographic_location(@publication)
     apply_tag_creatable_associations(@publication)
 
     if @publication.save
@@ -73,5 +72,41 @@ class PublicationsController < ApplicationController
     publication.apply_weather_parameter_ids_with_tag_creation!(p[:weather_parameter_ids])
     publication.apply_medical_condition_ids_with_tag_creation!(p[:medical_condition_ids])
     publication.apply_statistical_method_ids_with_tag_creation!(p[:statistical_method_ids])
+  end
+
+  def sanitize_geographic_location(publication)
+    p = params[:publication] || {}
+
+    raw = p[:geographic_location_id].presence || p[:geographic_location_name].presence
+
+    # empty or explicit "0" -> unset
+    if raw.blank? || raw.to_s == "0"
+      publication.geographic_location_id = nil
+      return
+    end
+
+    # numeric -> use as id if it exists
+    if raw.to_s.match?(/\A\d+\z/)
+      id = raw.to_i
+      publication.geographic_location_id = GeographicLocation.exists?(id) ? id : nil
+      return
+    end
+
+    # otherwise treat as a name: find or create
+    name = raw.to_s.strip
+    if name.present?
+      geo = GeographicLocation.find_or_create_by(name: name)
+      publication.geographic_location_id = geo.id
+    else
+      publication.geographic_location_id = nil
+    end
+  end
+
+  def habtm_keys
+    %i[air_pollutant_ids weather_parameter_ids medical_condition_ids statistical_method_ids]
+  end
+
+  def assignment_attrs
+    publication_params.except(*habtm_keys)
   end
 end
