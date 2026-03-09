7ce9556d524fead6ce02371aa004d36902ca8496
DjNoopNoop <rajeevan.anoop@gmail.com>
2026-03-09 13:54:08 -0400

    re-removed imports from index.js

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,8 +2,3 @@
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
-
-import TomSelectController from "./tom_select_controller"
-application.register("tom-select", TomSelectController)
-import DataTableController from "./datatable_controller"
-application.register("datatable", DataTableController)
